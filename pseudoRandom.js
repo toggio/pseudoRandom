efea7e3cc058555f6c02f29bb5e6030e7a1c275d
Luca Soltoggio <toggio78@gmail.com>
2024-03-18 15:38:06 +0100

    Update pseudoRandom.js

--- pseudoRandom.js
+++ pseudoRandom.js
@@ -1,3 +1,45 @@
+/*
+ * Universal Cross-Platform Multi-Language Pseudo Random Number Generator (pseudoRandom) v0.9
+ *
+ * A Set of libraries for Pseudo-Random Number Generation across environments
+ *
+ * X-PRNG is an advanced set of libraries designed to provide developers with tools
+ * for generating pseudo-random numbers across various computing environments. It
+ * supports a wide range of applications, from statistical analysis and data simulation
+ * to secure cryptographic functions. These libraries offer a simple yet powerful
+ * interface for incorporating high-quality, pseudo-random numbers into applications
+ * written in different programming languages, ensuring consistency and reliability
+ * across platforms. 
+ *
+ * Copyright (C) 2024 under GPL v. 2 license
+ * 18 March 2024
+ *
+ * @author Luca Soltoggio
+ * https://www.lucasoltoggio.it
+ * https://github.com/toggio/X-PRNG
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+ 
+/*
+ *
+ * PseudoRandom Number Generator JavaScript Class
+ *
+ * This class implements a pseudo-random number generator (PRNG) using a linear
+ * congruential generator (LCG) algorithm. 
+ *
+ */
+
 class pseudoRandom {
 	/**
 	 * Constructor to initialize the PRNG with an optional seed.
